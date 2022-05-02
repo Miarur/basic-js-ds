c0612c90376e4c1e43ee253b20c11da19b7efc94
miarur <deadheartedfo@yahoo.com>
2022-05-03 02:48:40 +0300

    feat: done stack

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor() {
+    this.stack = [];
+  }
+  push(item) {
+    this.stack.push(item);
+    console.log(this.stack);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let del = this.stack[this.stack.length - 1];
+    this.stack.pop();
+    return del;
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    console.log(this.stack[this.stack.length - 1])
+    return this.stack[this.stack.length - 1];
   }
 }
 
